4bd5411155377a472f0da8b4b20f8de56b22cb5f
akhtarshahbaz1 <akhtar.shahbaz1@gmail.com>
2024-08-28 11:53:32 +0530

    factorymethod design pattern added

---
+++ demos/factorymethod/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using factorymethod;
+
+Console.WriteLine("Hello, World!");
+// The client code works with an instance of a concrete creator
+// The CreateProduct will return the actual product instance via the product interface
+
+//PlatinumFactory CreateProduct method will return an instance of Platinum Product via the CreditCard interface
+ICreditCard creditCard = new PlatinumFactory().CreateProduct();
+if (creditCard != null)
+{
+    Console.WriteLine("Card Type : " + creditCard.GetCardType());
+    Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+    Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+}
+else
+{
+    Console.Write("Invalid Card Type");
+}
+Console.WriteLine("--------------");
+//MoneyBackFactory CreateProduct method will return an instance of Platinum Product via the CreditCard interface
+creditCard = new MoneyBackFactory().CreateProduct();
+if (creditCard != null)
+{
+    Console.WriteLine("Card Type : " + creditCard.GetCardType());
+    Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+    Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+}
+else
+{
+    Console.Write("Invalid Card Type");
+}
+Console.ReadLine();
